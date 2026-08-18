41321a53343b56518ca39a295bdcf36f65fc9c99
GeraldAres <Geraldbeyou@gmail.com>
2026-08-18 15:41:09 +0800

    Add(Folder): added two subject folders

---
+++ cs345/test.cs
@@ -0,0 +1,18 @@
+internal class test {
+    private static void Main(){
+        Console.WriteLine("Hello World");
+    }
+}
+
+/*
+
+
+Folder Project: Act1F1
+- choose the .net Framework file not the other one
+-  for the framework choose the .NET frameowrk 4.7.2 
+
+
+
+
+
+*/

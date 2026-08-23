6737d4934080e4fcbebcb838fcb29387fdc748d7
GeraldAres <Geraldbeyou@gmail.com>
2026-08-23 18:37:29 +0800

    Added Automata and Python Assignment

---
+++ cs365/TESTING/helo.py
@@ -0,0 +1,13 @@
+# Problem 1
+# TODO: write the program here
+x = 34 - 23
+y = "Hello"
+z = 3.45
+newWord = " World"
+if z == 3.45 and y == "Hello":
+    x += 1
+    print(x)
+    print(y + newWord) 
+else:
+    print("Wrong")
+
